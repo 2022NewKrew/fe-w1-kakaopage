4d20c9cbeb1f2b1bc1a6ea2604d5544d41d229b3
brad.ley <brad.ley@kakaocorp.com>
2021-12-27 20:21:50 +0900

    refactor to simplify tab function code

--- public/javascripts/header-script.js
+++ public/javascripts/header-script.js
@@ -5,80 +5,24 @@ function removeAllChild(element) {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    const homeNavBtnEl = document.querySelector('#low-header-nav-home')
-    const webtoonNavBtnEl = document.querySelector('#low-header-nav-webtoon')
-    const webnovelNavBtnEl = document.querySelector('#low-header-nav-webnovel')
-    const movieNavBtnEl = document.querySelector('#low-header-nav-movie')
-    const broadcastNavBtnEl = document.querySelector('#low-header-nav-broadcast')
-    const bookNavBtnEl = document.querySelector('#low-header-nav-book')
-    
     const mainContainerEl = document.querySelector('.main-container')
-    const homeContentEl = document.querySelector('.home-content')
-    const webtoonContentEl = document.querySelector('.webtoon-content')
-    const webnovelContentEl = document.querySelector('.webnovel-content')
-    const movieContentEl = document.querySelector('.movie-content')
-    const broadcastContentEl = document.querySelector('.broadcast-content')
-    const bookContentEl = document.querySelector('.book-content')
+    const navBtnEls = document.querySelectorAll('.low-header-nav-item')
+    const contentEls = document.querySelectorAll('.content')
     
-    let selectedNavEl = homeNavBtnEl;
+    let selectedIdx = 0
     
-    mainContainerEl.removeChild(webtoonContentEl)
-    mainContainerEl.removeChild(webnovelContentEl)
-    mainContainerEl.removeChild(movieContentEl)
-    mainContainerEl.removeChild(broadcastContentEl)
-    mainContainerEl.removeChild(bookContentEl)
+    removeAllChild(mainContainerEl)
+    mainContainerEl.appendChild(contentEls[selectedIdx])
+    navBtnEls[selectedIdx].classList.add('selected-low-header-nav-item')
     
-    homeNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        homeNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = homeNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(homeContentEl)
-    })
-    
-    webtoonNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        webtoonNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = webtoonNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(webtoonContentEl)
-    })
-    
-    webnovelNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        webnovelNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = webnovelNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(webnovelContentEl)
-    })
-    
-    movieNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        movieNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = movieNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(movieContentEl)
-    })
-    
-    broadcastNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        broadcastNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = broadcastNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(broadcastContentEl)
-    })
-    
-    bookNavBtnEl.addEventListener('click', () => {
-        selectedNavEl.classList.remove('selected-low-header-nav-item')
-        bookNavBtnEl.classList.add('selected-low-header-nav-item')
-        selectedNavEl = bookNavBtnEl
-        
-        removeAllChild(mainContainerEl)
-        mainContainerEl.appendChild(bookContentEl)
+    navBtnEls.forEach((navBtnEl, idx) => {
+        navBtnEl.addEventListener('click', () => {
+            navBtnEls[selectedIdx].classList.remove('selected-low-header-nav-item')
+            navBtnEl.classList.add('selected-low-header-nav-item')
+            selectedIdx = idx
+            
+            removeAllChild(mainContainerEl)
+            mainContainerEl.appendChild(contentEls[idx])
+        })
     })
 })
