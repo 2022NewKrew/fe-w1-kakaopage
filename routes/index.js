dc56a9c87d6bfd90c8d5186b6928c4bbdbc318bf
brad.ley <brad.ley@kakaocorp.com>
2021-12-27 17:31:20 +0900

    add html code for header

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index.html')
+    res.render('kakaopage.html')
 })
 
 module.exports = router
