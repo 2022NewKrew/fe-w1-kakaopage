c22b043474a482b1fd222133447fa72bc89ea0d2
terajh <terajoohyun@ajou.ac.kr>
2021-12-27 18:38:51 +0900

    feat: add header tag

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+const kakao_content_nav = document.querySelector("nav");
+const kakao_header = document.querySelector("header");
+const kakao_content = document.querySelector("#main_content>div.content");
+
+function init() {
+    // kakao_content_nav.style("position: ");
+    // console.log("init kakaopage clone");
+}
