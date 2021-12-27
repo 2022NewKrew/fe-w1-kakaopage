6a04821e0c73bfb7975091f649337f1d251dc476
terajh <terajoohyun@ajou.ac.kr>
2021-12-27 19:49:04 +0900

    feat: add content nav (클릭 이벤트 콜백 추가)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,17 @@
 const kakao_content_nav = document.querySelector("nav");
 const kakao_header = document.querySelector("header");
 const kakao_content = document.querySelector("#main_content>div.content");
+const kakao_content_nav_ul = document.querySelector("nav.content_nav>ul");
+
+let cur_content_nav_li = document.querySelector("li.clicked") || null;
 
 function init() {
-    // kakao_content_nav.style("position: ");
-    // console.log("init kakaopage clone");
+    kakao_content_nav_ul.addEventListener("click", (e) => {
+        console.dir(e.target.parentNode);
+        if (cur_content_nav_li) {
+            cur_content_nav_li.classList.remove("clicked");
+        }
+        cur_content_nav_li = e.target.parentNode;
+        cur_content_nav_li.classList.add("clicked");
+    });
 }
