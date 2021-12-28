b424f6a09850abec81b178f294eb9cff7b269aa3
lin.s <lin.s@kakaocorp.com>
2021-12-28 20:17:15 +0900

    Update daily ranking

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
+// banner carousel
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide_item");
   if (n > slides.length) {
-    slideIndex = 1
-  }    
+    slideIndex = 1;
+  }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "flex";  
-  slides[slideIndex-1].style.alignItems = "center"; 
+  slides[slideIndex - 1].style.display = "flex";
+  slides[slideIndex - 1].style.alignItems = "center";
+}
+
+// 요일 연재 TOP
+// 웹툰 복제하기
+let dailyContainer = document.getElementsByClassName(
+  "daily-ranking-contents-container"
+)[0];
+
+for (i = 0; i < 9; i++) {
+  let dailytop = document.getElementsByClassName("daily-ranking-content")[0];
+  dailyContainer.appendChild(dailytop.cloneNode(true));
 }
