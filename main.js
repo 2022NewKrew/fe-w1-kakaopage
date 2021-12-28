9a1cd3363d060ea3cf975a6232f29f048c95f80e
Chae Lin Shin <chaelinshin96@gmail.com>
2021-12-28 19:59:26 +0900

    [lin.s] 1주차 미션 PR - 1 (#5)
    * Add header
    
    * Add navigation
    
    * Add webtoon menu selection
    
    * Add carousel
    
    * Add webtoon menus
    
    * Add banner carousel
    
    * Add js file (transfer script to js file)
    
    * Update js variable (var -> let)
    
    * Add daily ranking header
    
    * Fix webtoon menu alignment
    
    Co-authored-by: lin.s <lin.s@kakaocorp.com>

---
+++ main.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide_item");
+  if (n > slides.length) {
+    slideIndex = 1
+  }    
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  slides[slideIndex-1].style.alignItems = "center"; 
+}
