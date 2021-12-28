c5c4f292b4fbf3d43f95da0311ac9c0abb2fa49e
dlguswo333 <dlguswo002@gmail.com>
2021-12-28 12:35:15 +0900

    Separate js code and Add dummy tab

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # fe-w1-kakaopage
 
+## How to Run
+Run simple local HTTP server, such as Python `http.server`, or
+Node.js `http-server`.
+
 ## KakaoPage Tree
 
 ``` 
 main.html
-└─┬ home.html
+└─┬ home.html (-> dummy-tab.html)
   │ 
   ├ webtoon.html
   │ 
-  ├ webfic.html
+  ├ webfic.html (-> dummy-tab.html)
   │ 
-  ├ movie.html
+  ├ movie.html (-> dummy-tab.html)
   │ 
-  ├ broadcast.html
+  ├ broadcast.html (-> dummy-tab.html)
   │ 
-  └ book.html
+  └ book.html (-> dummy-tab.html)
 ```
---
+++ static/main.js
@@ -0,0 +1,64 @@
+function add_tab_active(tab){
+  let tabs=document.getElementById("header-tabs");
+  Array.from(tabs.children).some((ele)=>{
+    if(ele.classList.contains("header-tab-active")){
+      ele.classList.remove("header-tab-active");
+      return true;
+    }
+    return false;
+  });
+  tab.classList.add("header-tab-active");
+}
+async function load_tab(url){
+  return await (await fetch(url)).text();
+}
+function home_tab(){
+  add_tab_active(document.getElementById("home-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("dummy.html").then((text)=>{
+    container.innerHTML=text;
+  });
+}
+function webtoon_tab(){
+  add_tab_active(document.getElementById("webtoon-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("webtoon.html").then((text)=>{
+    container.innerHTML=text;
+  });
+
+}
+function webfic_tab(){
+  add_tab_active(document.getElementById("webfic-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("dummy.html").then((text)=>{
+    container.innerHTML=text;
+  });
+}
+function movie_tab(){
+  add_tab_active(document.getElementById("movie-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("dummy.html").then((text)=>{
+    container.innerHTML=text;
+  });
+}
+function broadcast_tab(){
+  add_tab_active(document.getElementById("broadcast-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("dummy.html").then((text)=>{
+    container.innerHTML=text;
+  });
+}
+function book_tab(){
+  add_tab_active(document.getElementById("book-tab"));      
+  let container=document.getElementById("contents");
+  load_tab("dummy.html").then((text)=>{
+    container.innerHTML=text;
+  });
+}
+document.getElementById("home-tab").onclick=home_tab;
+document.getElementById("webtoon-tab").onclick=webtoon_tab;
+document.getElementById("webfic-tab").onclick=webfic_tab;
+document.getElementById("movie-tab").onclick=movie_tab;
+document.getElementById("broadcast-tab").onclick=broadcast_tab;
+document.getElementById("book-tab").onclick=book_tab;
+home_tab();
