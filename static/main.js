9b6c725dd5741a516703c7affaa21b3aaf575b94
dlguswo333 <dlguswo002@gmail.com>
2021-12-28 20:37:55 +0900

    Reflect reviews
    Use camelCase naming
    Remove duplicated codes
    Move frequently used variables to global scope
    Wrap default codes in init function

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
-function add_tab_active(tab){
-  let tabs=document.getElementById("header-tabs");
+const tabs=document.getElementById("header-tabs");
+
+function markTabActive(tab){
   Array.from(tabs.children).some((ele)=>{
     if(ele.classList.contains("header-tab-active")){
       ele.classList.remove("header-tab-active");
@@ -9,58 +10,29 @@ function add_tab_active(tab){
   });
   tab.classList.add("header-tab-active");
 }
-async function load_tab(url){
-  return await (await fetch(url)).text();
+async function loadTab(url){
+  const text=await (await fetch(url)).text();
+  const container=document.getElementById("contents");
+  container.innerHTML=text;
 }
-function home_tab(){
-  add_tab_active(document.getElementById("home-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("dummy.html").then((text)=>{
-    container.innerHTML=text;
+function init(){
+  const tabIdToHtml={
+    "home-tab": "dummy.html",
+    "webtoon-tab": "webtoon.html",
+    "webfic-tab": "dummy.html",
+    "movie-tab": "dummy.html",
+    "broadcast-tab": "dummy.html",
+    "book-tab": "dummy.html"
+  };
+  tabs.addEventListener('click', (e)=>{
+    const tabId=e.target.getAttribute("data-tab-id");
+    const tab=document.getElementById(tabId);
+    markTabActive(tab);
+    loadTab(tabIdToHtml[tabId]);
   });
+  // NOTE Temporarily set webtoon tab as default for convenience :)
+  // document.getElementById("home-tab").click();
+  document.getElementById("webtoon-tab").click();
 }
-function webtoon_tab(){
-  add_tab_active(document.getElementById("webtoon-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("webtoon.html").then((text)=>{
-    container.innerHTML=text;
-  });
 
-}
-function webfic_tab(){
-  add_tab_active(document.getElementById("webfic-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("dummy.html").then((text)=>{
-    container.innerHTML=text;
-  });
-}
-function movie_tab(){
-  add_tab_active(document.getElementById("movie-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("dummy.html").then((text)=>{
-    container.innerHTML=text;
-  });
-}
-function broadcast_tab(){
-  add_tab_active(document.getElementById("broadcast-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("dummy.html").then((text)=>{
-    container.innerHTML=text;
-  });
-}
-function book_tab(){
-  add_tab_active(document.getElementById("book-tab"));      
-  let container=document.getElementById("contents");
-  load_tab("dummy.html").then((text)=>{
-    container.innerHTML=text;
-  });
-}
-document.getElementById("home-tab").onclick=home_tab;
-document.getElementById("webtoon-tab").onclick=webtoon_tab;
-document.getElementById("webfic-tab").onclick=webfic_tab;
-document.getElementById("movie-tab").onclick=movie_tab;
-document.getElementById("broadcast-tab").onclick=broadcast_tab;
-document.getElementById("book-tab").onclick=book_tab;
-// home_tab();
-// NOTE Temporarily set webtoon tab as default for convenience :)
-webtoon_tab();
+init();
