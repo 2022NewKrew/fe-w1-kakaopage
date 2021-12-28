6e6f2dc43748edc4c751463b4e60a7f9b523d6f9
dobby.shin <dobby.shin@kakaocorp.com>
2021-12-28 14:51:19 +0900

    ADD : navbar layout & style

---
+++ client/assets/assets.js
@@ -0,0 +1,26 @@
+export default 
+{
+    "navbar": {
+        "menus": [
+            "https://static-page.kakao.com/static/pc/menu_home.svg?156813c20605e12311c90bae39f38c31",
+            "https://static-page.kakao.com/static/pc/menu_toon.svg?fd6837bff2e823e13c693320961cc5a8",
+            "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
+            "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
+            "https://static-page.kakao.com/static/pc/menu_broadcast.svg?549cf0553505cda3d418aa684f358c18",
+            "https://static-page.kakao.com/static/pc/menu_book.svg?eb9b97c2528955f1e6cf788c6fe7e504",
+        ],
+    },
+    "main": {
+      "menus": [
+        "홈",
+        "요일연재",
+        "웹툰",
+        "소년",
+        "드라마",
+        "로맨스",
+        "로판",
+        "액션무협",
+        "BL",
+      ]
+    }
+}
