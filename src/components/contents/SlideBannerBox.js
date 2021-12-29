ed2fb1c62bb9888cba40dc9a386710ad1d8c9a3f
edward.kk <edward.kk@kakaocorp.com>
2021-12-29 10:34:57 +0900

    feat. slideBanner에 백그라운드 그림자 추가

--- src/components/contents/SlideBannerBox.js
+++ src/components/contents/SlideBannerBox.js
@@ -3,13 +3,16 @@ export const SlideBannerBox = () => {
   $root.className = "slideBannerBox";
 
   $root.innerHTML = `
-    <img
-      src="//dn-img-page.kakao.com/download/resource?kid=bxer2r/hzhOhiglp7/6XVB5RgYtBsV1zz7MxoeX1"
-      data-src="//dn-img-page.kakao.com/download/resource?kid=bxer2r/hzhOhiglp7/6XVB5RgYtBsV1zz7MxoeX1"
-      alt="ㅋㅋㅋ웹툰아카데미 카카페 데뷔반 2기"
-      draggable="false"
-      style="width: 720px; display: block"
-    />
+    <div>
+      <img
+        class="slideBannerBox__img"
+        src="//dn-img-page.kakao.com/download/resource?kid=bxer2r/hzhOhiglp7/6XVB5RgYtBsV1zz7MxoeX1"
+        data-src="//dn-img-page.kakao.com/download/resource?kid=bxer2r/hzhOhiglp7/6XVB5RgYtBsV1zz7MxoeX1"
+        alt="ㅋㅋㅋ웹툰아카데미 카카페 데뷔반 2기"
+        draggable="false"
+      />
+      <div class="slideBannerBox__shadow"></div>
+    </div>
     <div>
       <div class="slideBannerBox__content">
         <p>ㅋㅋㅋ웹툰 아카데미</p>
