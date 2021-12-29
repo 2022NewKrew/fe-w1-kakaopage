bbe0c88783580ce6aadb4828cc3208a58aed7455
lin.s <lin.s@kakaocorp.com>
2021-12-29 19:42:58 +0900

    Update 요일연재TOP - JSON data

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// 초기화 함수
+function init() {
+  const menus = document.getElementById("navigation");
+
+  // 초기 페이지 로딩
+  loadMenu("webtoon-tab");
+
+  // 각 메뉴별 이벤트리스너 추가
+  menus.addEventListener("click", (e) => {
+    const menuId = e.target.getAttribute("data-tab-id");
+    if (menuId) {
+      const menu = document.getElementById(menuId).parentNode;
+      makeMenuActive(menu);
+      loadMenu(menuId);
+    } else {
+      return;
+    }
+  });
+}
+
+init();
+
+// 메뉴 관련
 function makeMenuActive(menu) {
   const menus = document.getElementsByClassName("menu");
   Array.from(menus).forEach((e) => {
@@ -23,42 +46,132 @@ async function loadMenu(menu) {
   const text = await file.text();
   home.innerHTML = "";
   await home.insertAdjacentHTML("afterbegin", text);
-
-  webtoonInit();
+  if (menu === "webtoon-tab") {
+    webtoonInit();
+  }
 }
 
-function init() {
-  const menus = document.getElementById("navigation");
-
-  // 초기 페이지
-  loadMenu("webtoon-tab");
-
-  // 각 메뉴별 이벤트리스너 추가
-  menus.addEventListener("click", (e) => {
-    const menuId = e.target.getAttribute("data-tab-id");
-    if (menuId) {
-      const menu = document.getElementById(menuId).parentNode;
-      makeMenuActive(menu);
-      loadMenu(menuId);
-    } else {
-      return;
-    }
-  });
-}
-
-init();
-
+// 웹툰 페이지 초기화
 function webtoonInit() {
-  // 각 장르별 이벤트리스너 추가
+  // 장르 setup
   const genres = document.getElementById("genres");
+  makeGenre(genres);
+
+  makeTopRanking();
+
+  showSlides(1);
+}
+
+// 장르 관련
+async function makeGenre(genres) {
+  const genreFile = await fetch("data/genres.json");
+  const genreList = (await genreFile.json()).genres;
+  await genreList.forEach((data) => {
+    genres.insertAdjacentHTML(
+      "beforeend",
+      `<li class="genre">${data.title}</li>`
+    );
+  });
+  // 각 장르별 이벤트리스너 추가
   genres.addEventListener("click", (e) => {
     const genre = e.target;
     makeGenreActive(genre);
   });
+  genres.firstElementChild.classList.add("active-genre");
+}
 
-  topRanking();
+// 장르 메뉴 선택시 색 변경
+function makeGenreActive(genre) {
+  const genres = document.getElementsByClassName("genre");
+  Array.from(genres).forEach((e) => {
+    if (e.classList.contains("active-genre")) {
+      e.classList.remove("active-genre");
+      return;
+    }
+  });
+  genre.classList.add("active-genre");
+}
 
-  showSlides(1);
+// 요일 연재 TOP 컴포넌트
+async function makeTopRanking() {
+  const weekTab = document.getElementById("daily-ranking-tabs-container");
+  const weekFile = await fetch("data/week.json");
+  const weekList = (await weekFile.json()).week;
+  // 각 요일 탭 추가
+  weekList.forEach((data) => {
+    weekTab.insertAdjacentHTML(
+      "beforeend",
+      `<li class="daily-ranking-tab" id=${data.english}>${data.weekday}</li>`
+    );
+  });
+  // 각 요일 탭에 이벤트 리스너 추가
+  weekTab.addEventListener("click", function (e) {
+    const tabs = document.getElementsByClassName("daily-ranking-tab");
+    Array.from(tabs).forEach((tab) => {
+      if (tab.classList.contains("active-weekday")) {
+        tab.classList.remove("active-weekday");
+        return;
+      }
+    });
+    e.target.classList.add("active-weekday");
+    fetchTopRankingWebtoons(e.target.id);
+  });
+  // 디폴트 요일은 월요일
+  weekTab.firstChild.classList.add("active-weekday");
+  fetchTopRankingWebtoons("monday");
+}
+
+async function makeWeekTabs() {}
+
+async function fetchTopRankingWebtoons(weekday) {
+  let dailyContainer = document.getElementsByClassName(
+    "daily-ranking-contents-container"
+  )[0];
+  dailyContainer.innerHTML = "";
+
+  const dailyWebtoonFile = await fetch("data/daily-webtoon.json");
+  const dailyWebtoonList = (await dailyWebtoonFile.json())[weekday];
+
+  dailyWebtoonList.forEach((data) => {
+    dailyContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+        <a href="#" class="daily-ranking-content">
+        <div class="daily-ranking-content-container">
+          <div class="daily-ranking-image-container">
+            <img
+              src=${data.img}
+              alt="${data.title}"
+            />
+          </div>
+          <div class="daily-ranking-box">
+            <p class="daily-ranking-box-left">${data.left}</p>
+            ${
+              data.clock
+                ? `<img src="https://static-page.kakao.com/static/common/bmbadge_waitfree.svg?53cf25c84253dee8d32e66da7524dbaf" />`
+                : `<p class="daily-ranking-box-right">웹툰</p>`
+            }
+          </div>
+        </div>
+        <div>
+          <p class="daily-ranking-title">${data.title}</p>
+          <div class="daily-ranking-people">
+            ${
+              data.fifteen
+                ? `<img src="https://static-page.kakao.com/static/common/icon_15.png?ccf202bf79001052f43af077a0947e74" alt="15세 이용가`
+                : data.new
+                ? `<img src="https://static-page.kakao.com/static/common/icon_new.svg?4ae84a0f972e30119fb6fcfbb2f59bf9" alt="new"/>`
+                : ""
+            }
+            <img
+              src="https://static-page.kakao.com/static/common/icon_read_count.png?817b1f83aa0dd8de232a68ac82efd871"
+            />${data.people}만명
+          </div>
+        </div>
+      </a>
+    `
+    );
+  });
 }
 
 // banner carousel
@@ -85,28 +198,4 @@ function showSlides(slideNum) {
   });
   slides[slideIndex - 1].style.display = "flex";
   slides[slideIndex - 1].style.alignItems = "center";
-}
-
-// 장르 메뉴 선택시 색 변경
-function makeGenreActive(genre) {
-  const genres = document.getElementsByClassName("genre");
-  Array.from(genres).forEach((e) => {
-    if (e.classList.contains("active-genre")) {
-      e.classList.remove("active-genre");
-      return;
-    }
-  });
-  genre.classList.add("active-genre");
-}
-
-// 요일 연재 TOP 컴포넌트 추가
-function topRanking() {
-  let dailyContainer = document.getElementsByClassName(
-    "daily-ranking-contents-container"
-  )[0];
-
-  for (i = 0; i < 9; i++) {
-    let dailytop = document.getElementsByClassName("daily-ranking-content")[0];
-    dailyContainer.appendChild(dailytop.cloneNode(true));
-  }
 }
