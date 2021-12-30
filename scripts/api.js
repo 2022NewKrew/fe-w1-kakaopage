182c724ad7180ed488821e8149abb7a01eea5af5
fleek-moon <fleek.moon@kakaocorp.com>
2021-12-30 14:46:56 +0900

    chore : Add API_ENDPOINT

--- scripts/api.js
+++ scripts/api.js
@@ -1,6 +1,8 @@
+const API_ENDPOINT = "data/";
+
 export const getAPI = async (url) => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(API_ENDPOINT + url);
     return await response.json();
   } catch (e) {
     throw e;
