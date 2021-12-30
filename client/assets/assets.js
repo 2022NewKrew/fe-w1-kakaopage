cddff01edf255f0b73682cbb7f8bacad797c2e30
Dongyun Shin <77421871+dth12@users.noreply.github.com>
2021-12-30 18:20:40 +0900

    Feature/section (#26)
    * [dobby] ADD : carousel & webtoon list for days
    
    * [dobby] ADD : Remove new line
    
    Co-authored-by: dobby.shin <dobby.shin@kakaocorp.com>

--- client/assets/assets.js
+++ client/assets/assets.js
@@ -1,26 +1,53 @@
-export default 
-{
-    "navbar": {
-        "menus": [
-            "https://static-page.kakao.com/static/pc/menu_home.svg?156813c20605e12311c90bae39f38c31",
-            "https://static-page.kakao.com/static/pc/menu_toon.svg?fd6837bff2e823e13c693320961cc5a8",
-            "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
-            "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
-            "https://static-page.kakao.com/static/pc/menu_broadcast.svg?549cf0553505cda3d418aa684f358c18",
-            "https://static-page.kakao.com/static/pc/menu_book.svg?eb9b97c2528955f1e6cf788c6fe7e504",
-        ],
-    },
-    "main": {
+export default {
+  "navbar": {
       "menus": [
-        "홈",
-        "요일연재",
-        "웹툰",
-        "소년",
-        "드라마",
-        "로맨스",
-        "로판",
-        "액션무협",
-        "BL",
-      ]
-    }
+          "https://static-page.kakao.com/static/pc/menu_home.svg?156813c20605e12311c90bae39f38c31",
+          "https://static-page.kakao.com/static/pc/menu_toon.svg?fd6837bff2e823e13c693320961cc5a8",
+          "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
+          "https://static-page.kakao.com/static/pc/menu_novel.svg?417f894a74c6cd5334b4a84cfa470d55",
+          "https://static-page.kakao.com/static/pc/menu_broadcast.svg?549cf0553505cda3d418aa684f358c18",
+          "https://static-page.kakao.com/static/pc/menu_book.svg?eb9b97c2528955f1e6cf788c6fe7e504",
+      ],
+  },
+  "section": {
+    "menus": [
+      "홈",
+      "요일연재",
+      "웹툰",
+      "소년",
+      "드라마",
+      "로맨스",
+      "로판",
+      "액션무협",
+      "BL",
+    ],
+    "carousel": [
+      "https://dn-img-page.kakao.com/download/resource?kid=crO1D/hzaceG5FXr/khBZ22KgbtsyZr4LO8PILk",
+      "https://dn-img-page.kakao.com/download/resource?kid=cjPHF/hzhOnwa6jJ/G8vv6traaUpcWYE7Pnl6jk",
+      "https://dn-img-page.kakao.com/download/resource?kid=bOw4pk/hzhOgcf9a2/8I6xuyASB6Zlq3AGbkocD0",
+      "https://dn-img-page.kakao.com/download/resource?kid=bGorEd/hzhOfksubB/UgOlCAJMG6ntK2u4V4oAzk",
+      "https://dn-img-page.kakao.com/download/resource?kid=bdp2gx/hyQ9Lcq7ul/WYCSXf2jAlDD7k39aO8yh1",
+    ]
+  },
+  "article": {
+    "days": [
+      "월",
+      "화",
+      "수",
+      "목",
+      "금",
+      "토",
+      "일",
+      "전체",
+    ],
+    "daysWebtoonList": [
+      {
+        "title": "샬롯에게는 다섯 명의 제자가 있다",
+        "src": "https://dn-img-page.kakao.com/download/resource?kid=dy1Oc2/hywgI1bMRS/7QsDTWgZtp8XA30K0Si1u0&filename=th2",
+        "description": "대마법사 샬롯의 제자들이 이상하다?",
+        "numOfView": "106.6",
+        "author": "용용",
+      }
+    ]
+  } 
 }
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,8 +2,51 @@ import assets from '../assets/assets.js'
 
 const navBar = document.querySelector('nav')
 assets.navbar.menus.forEach(menuImgSrc => {
-  const menuDiv = document.createElement('img')
-  menuDiv.classList.add('nav-menu')
-  menuDiv.src = menuImgSrc
-  navBar.appendChild(menuDiv)
+  const menuImg = document.createElement('img')
+  menuImg.classList.add('nav-menu')
+  menuImg.src = menuImgSrc
+  navBar.appendChild(menuImg)
+})
+
+const webtoonMenus = document.querySelector('.webtoon-menus')
+assets.section.menus.forEach(sectionMenu => {
+  const menuDiv = document.createElement('li')
+  menuDiv.classList.add('webtoon-menu')
+  menuDiv.innerText = sectionMenu
+  if (sectionMenu === '홈') menuDiv.style = 'color: black;'
+  webtoonMenus.appendChild(menuDiv)
+})
+
+const webtoonCarouselContainer = document.querySelector('.webtoon-carousel')
+assets.section.carousel.forEach(carouselImgSrc => {
+  const carouselImg = document.createElement('img')
+  carouselImg.src = carouselImgSrc
+  carouselImg.width = 750
+  carouselImg.height = 480
+  webtoonCarouselContainer.prepend(carouselImg)
+})
+
+const webtoonDayList = document.querySelector('.days-list')
+const today = new Date()
+let checkedDate = (today.getDay() + 6) % 7
+const clickWebtoonDate = function (event) {
+  const beforeClickedDate = webtoonDayList.querySelector(`div:nth-child(${checkedDate + 1})`)
+  console.log(beforeClickedDate)
+  beforeClickedDate.classList.remove('checked-date')
+  checkedDate = Number(event.target.dataset.date)
+  event.target.classList.add('checked-date')
+}
+
+assets.article.days.forEach((day, idx) => {
+  const dayDiv = document.createElement('div')
+  dayDiv.innerText = day
+  dayDiv.dataset.date = idx
+  if (checkedDate === idx) dayDiv.classList.add('checked-date')
+  dayDiv.addEventListener('click', clickWebtoonDate)
+  webtoonDayList.appendChild(dayDiv)
+})
+
+assets.article.daysWebtoonList.forEach(webtoonInfo => {
+  const webtoonInfoDiv = document.createElement('div')
+  const webtoonImg = document.createElement('img')
 })
