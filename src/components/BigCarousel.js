0026217a163ff4c2034a81112ea957ce10ce2449
vvvpiano <vvvpiano60@gmail.com>
2021-12-30 14:36:28 +0900

    feat: bigCarousel 버튼 클릭시 움직임 구현

--- src/components/BigCarousel.js
+++ src/components/BigCarousel.js
@@ -1,3 +1,64 @@
+const renderCell = ({ thumbnail, description_title, tag_img, category, viewer, under_title}) => {
+    const cell = document.createElement("div");
+    cell.className = "carousel-cell";
+    cell.innerHTML = `
+        <img class="carousel-big-image" src=${thumbnail} alt=${description_title} />
+        <div class="big-image-info">
+            <div class="big-image-description">
+                <div>${description_title}</div>
+                <div> 이벤트 | ${category} | ${viewer}만명</div>
+            </div>
+            <div class="big-image-title">
+                <span>${under_title}</span>
+            </div>
+        </div>
+    `
+    return cell;
+}
+
+const renderContainer = (data) => {
+    const container = document.createElement("div");
+    container.className = "carousel-container";
+    data.forEach(cell_data => container.appendChild(renderCell(cell_data)));
+    return container;
+}
+
+const renderWindow = () => {
+    const carousel_window = document.createElement("div");
+    carousel_window.className = "carousel-window";
+    carousel_window.innerHTML = `
+        <div class="btn-wrapper prev">
+            <img class="prev-btn" src="https://static-page.kakao.com/static/pc/ic-banner-paging-back-nor.svg?85bef3b447d17ee7cbefa349c973fe56" />
+        </div>
+        <div class="btn-wrapper next">
+            <img class="next-btn" src="https://static-page.kakao.com/static/pc/ic-banner-paging-next-nor.svg?cf6a870397c04c13add6c27f1f735d93" />
+        </div>
+    `
+    return carousel_window;
+}
+
+const renderCarousel = (data) => {
+    const carousel_window = renderWindow();
+    const container = renderContainer(data);
+    const prev_btn = carousel_window.querySelector(".prev-btn");
+    const next_btn = carousel_window.querySelector(".next-btn");
+
+    carousel_window.insertBefore(container, carousel_window.children[1]);
+
+    let count = 0;
+    const clickCallBack = (direction) => {
+        return () => {
+            count = (count + direction) % data.length;
+            container.style.transform = `translateX(${count * 720}px)`;
+            container.style.transitionDuration = "500ms";
+        }
+    }
+    prev_btn.addEventListener("click", clickCallBack(1));
+    next_btn.addEventListener("click", clickCallBack(-1));
+    
+    return carousel_window;
+}
+
 export const BigCarousel = ({ data }) => {
 
     const target = document.createElement("div");
@@ -6,23 +67,7 @@ export const BigCarousel = ({ data }) => {
     const render = () => {
         if (!data || data.length === 0) return target;
 
-        const { thumbnail, description_title, tag_img, category, viewer, under_title} = data[0];
-        target.innerHTML = `
-            <div class="carousel">
-                <div>
-                    <img class="carousel-big-image" src=${thumbnail} alt=${description_title} />
-                    <div class="big-image-info">
-                        <div class="big-image-description">
-                            <div>${description_title}</div>
-                            <div> 이벤트 | ${category} | ${viewer}만명</div>
-                        </div>
-                        <div class="big-image-title">
-                            <span>${under_title}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `
+        target.appendChild(renderCarousel(data));
         return target;
     }
 
