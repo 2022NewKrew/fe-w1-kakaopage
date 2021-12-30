e74cd239a7b57942b62e647896c4a4f816c5af2d
fleek-moon <fleek.moon@kakaocorp.com>
2021-12-30 14:47:36 +0900

    style : Add empty page image

--- scripts/empty.js
+++ scripts/empty.js
@@ -2,6 +2,7 @@ export const createEmptyPage = () => {
   return `
               <div class='empty-page-container'>
                   <img src='./images/not-content.gif' />
+                  <img width="454" src="https://user-images.githubusercontent.com/50862052/147724907-9d0b2bdd-8e6a-4c2d-8597-0daa49ca5296.png">
               </div>
           `;
 };
