7467cf3f30dd47609eafe289f5ef55c2cc000724
h-yes-oo <hyesoo5115@naver.com>
2022-01-03 14:09:48 +0900

    change port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import router from "./router.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("src"));
 app.use(express.static("src/pages"));
